2ac294bd666bed7057dee4068367bd73239265b0
yhkim <yhkim@crescom.ai>
2022-05-13 11:22:07 +0900

    add argparse options

--- run_trainer.py
+++ run_trainer.py
@@ -1,13 +1,21 @@
 import os
 import yaml
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--config", help='config yaml path', default='config.yaml')
+parser.add_argument("-m", "--model", help='model name', default='TransUnet')
+parser.add_argument("-o", "--optimizer", help='optimizer name', default='SGD')
+parser.add_argument("-s", "--scheduler", help='scheduler_name', default='CosineAnnealingWarmRestarts')
+args = parser.parse_args()
 
 if __name__ == '__main__':
-    with open('config.yaml', 'r') as f:
+    with open(args.config, 'r') as f:
         config = yaml.load(f, yaml.FullLoader)
     os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
     os.environ["CUDA_VISIBLE_DEVICES"]= config['general']['cuda_device']
 
     from trainer import SegmentationTrainer
-    trainer = SegmentationTrainer(model_name='TransUnet', optimizer_name='SGD',
-                                scheduler_name='CosineAnnealingWarmRestarts', config=config)
+    trainer = SegmentationTrainer(model_name=args.model, optimizer_name=args.optimizer,
+                                scheduler_name=args.scheduler, test_mode=False, config=config)
     trainer.train()
